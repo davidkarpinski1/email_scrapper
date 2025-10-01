ed5ec4dfcb873da2307f839f940034e067f249e7
DavidKarpinski <karpinski.david@tuta.io>
2025-10-01 09:51:54 -0300

    feat(cli): allow passing URL as command-line argument

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import argparse
+
 def main():
-    print('Hello, World!')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', type=str, required=True)
+    args = parser.parse_args()
     
+    print(args.url)
 
 if __name__ == '__main__':
     main()
